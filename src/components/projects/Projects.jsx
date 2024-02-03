4ad2f9ee3bd66dc037699cedba7e342602d3aee7
ajithkumar2498 <ajibhc24@gmail.com>
2024-02-03 23:47:28 +0530

    new

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,39 +4,55 @@ import task from "../../images/task-project.jpeg"
 
 function Projects() {
   return <>
-  <section className="projects">
- <h2 classNameName="projects-Title">Projects</h2>
- <div class="container">
-    <div class="row">
-        <div class="col-md-4 col-sm-6">
-            <div class="box">
-                <img src={task}/>
-                <div class="box-content">
-                    <h3 class="title">Williamson</h3>
-                    <span class="post">Web designer</span>
-                    <ul class="icon">
-                        <li><a href="#"><i class="fa fa-search"></i></a></li>
-                        <li><a href="#"><i class="fa fa-link"></i></a></li>
-                    </ul>
+  <section className="projects-div">
+  <span className="Project-Title"><span className='project-bg'>Projects</span></span>  
+    <div className="cards">
+        <div className="card">
+            <div className="card-face front">
+                <img src={task} alt="" />
+                <h3>Task Tracker App</h3>
+            </div>
+            <div className="card-face back">
+                <h3>Task Tracker App</h3>
+                <p className="project-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Porro, error quisquam laborum quam </p>
+                <div className="project-link">
+                    <a href="https://github.com/ajithkumar2498/Task_tracker.git" target='_blank'><i className="fa-brands fa-github source" ></i></a>
+                    <a href="https://my-task-tracker-react-app.netlify.app/dashboard" target='_blank'><i className="fa-solid fa-link source" ></i></a>
                 </div>
             </div>
         </div>
-        <div class="col-md-4 col-sm-6">
-            <div class="box">
-                <img src={task}/>
-                <div class="box-content">
-                    <h3 class="title">Miranda Roy</h3>
-                    <span class="post">Web designer</span>
-                    <ul class="icon">
-                        <li><a href="#"><i class="fa fa-search"></i></a></li>
-                        <li><a href="#"><i class="fa fa-link"></i></a></li>
-                    </ul>
+        <div className="card">
+            <div className="card-face front">
+                <img src={task} alt="" />
+                <h3>Task Tracker App</h3>
+            </div>
+            <div className="card-face back">
+                <h3>Task Tracker App</h3>
+                <p className="project-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Porro, error quisquam laborum quam </p>
+                <div className="project-link">
+                    <a href="https://github.com/ajithkumar2498/Task_tracker.git" target='_blank'><i className="fa-brands fa-github source" ></i></a>
+                    <a href="https://my-task-tracker-react-app.netlify.app/dashboard" target='_blank'><i className="fa-solid fa-link source" ></i></a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="card-face front">
+                <img src={task} alt="" />
+                <h3>Task Tracker App</h3>
+            </div>
+            <div className="card-face back">
+                <h3>Task Tracker App</h3>
+                <p className="project-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Porro, error quisquam laborum quam </p>
+                <div className="project-link">
+                    <a href="https://github.com/ajithkumar2498/Task_tracker.git" target='_blank'><i className="fa-brands fa-github source" ></i></a>
+                    <a href="https://my-task-tracker-react-app.netlify.app/dashboard" target='_blank'><i className="fa-solid fa-link source" ></i></a>
                 </div>
             </div>
         </div>
     </div>
-</div>
-    
   </section>
 
   </>
