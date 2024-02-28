1e1e794172f88fd780e14e69449f382c62289814
ajithkumar2498 <ajibhc24@gmail.com>
2024-02-28 21:07:33 +0530

    new

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import './about.css'
+import college from "../../images/college.jpg"
 function About() {
   return<>
-  <section className='about'>
+  <section className='about-div'>
+    <span className="About-Title"><span className='about-bg'>About Me</span></span>  
+    <div className="about-container">
+       <div className="edu1">
+         <img src={college} className='edu1Image' alt="" />
+         <p className="content">
+            Lorem, ipsum dolor sit amet consectetur  accusamus velit ad quas quam soluta. Repudiandae reiciendis explicabo velit!         </p>
+       </div>
+       <div className="edu2">
+       <img src={college} className='edu1Image' alt="" />
+       <p className="content">
+       Lorem, ipsum dolor sit amet consectetur  accusamus velit ad quas quam soluta. Repudiandae reiciendis explicabo velit!   
+          </p>
+
+       </div>
+       <div className="edu3">
+       <img src={college} className='edu1Image' alt="" />
+       <p className="content">
+       Lorem, ipsum dolor sit amet consectetur  accusamus velit ad quas quam soluta. Repudiandae reiciendis explicabo velit!   
+          </p>
+
+       </div>
+    </div>
     
   </section>
   </>
